f4b23356d4431f7691381d1d12e4f2cf4ab0ae36
aghoribrahman <mohit.4033@gmail.com>
2023-08-30 00:45:29 +0530

    Routes Updated

--- src/App.js
+++ src/App.js
@@ -2,17 +2,24 @@ import './App.css';
 import NavBar from './components/NavBar';
 import { Component, useState } from 'react';
 import { NewsComponent } from './components/NewsComponent';
-import { BrowserRouter as Router, Route, Switch, Link, BrowserRouter } from 'react-router-dom';
+import { BrowserRouter , Routes, Route, Switch, Link } from 'react-router-dom';
 
 
 export class App extends Component{
   render(){
     return(
       <>
-
+        <BrowserRouter> 
         <NavBar />
-        <NewsComponent category='health' country='in'/>
-      
+        <Routes>
+          <Route path='/' element={<NewsComponent key='general' country='in' category='general' />}></Route>
+          <Route path='/business' element={<NewsComponent key='general' country='in' category='business' />}></Route>
+          <Route path='/technology' element={<NewsComponent key='technology' country='in' category='technology' />}></Route>
+          <Route path='/sports' element={<NewsComponent key='sports' country='in' category='sports' />}></Route>
+          <Route path='/entertainment' element={<NewsComponent key='entertainment' country='in' category='entertainment' />}></Route>
+          <Route path='/science' element={<NewsComponent key='science' country='in' category='science' />}></Route>
+          </Routes>       
+        </BrowserRouter>
       </>
     )
   }
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -13,7 +13,7 @@ render()
         <div className="collapse navbar-collapse" id="navbarNav">
             <ul className="navbar-nav">
             <li className="nav-item active">
-                <a className="nav-link" href="General">General<span className="sr-only">(current)</span></a>
+                <a className="nav-link" href="/">General<span className="sr-only">(current)</span></a>
             </li>
             <li className="nav-item">
                 <a className="nav-link" href="technology">technology</a>
